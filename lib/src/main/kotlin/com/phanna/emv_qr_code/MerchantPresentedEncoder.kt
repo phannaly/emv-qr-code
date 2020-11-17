5bd6636c6d429c8627e88efa50e762c5e914bd09
phanna <studentphanna@gmail.com>
2020-11-17 14:03:16 +0700

    add required field

--- lib/src/main/kotlin/com/phanna/emv_qr_code/MerchantPresentedEncoder.kt
+++ lib/src/main/kotlin/com/phanna/emv_qr_code/MerchantPresentedEncoder.kt
@@ -1,5 +1,7 @@
 package com.phanna.emv_qr_code
 
+import java.lang.Exception
+
 class MerchantPresentedEncoder {
     private val data = DataObjectTypes.data()
     private val dataField = mutableMapOf<String, String>()
@@ -20,12 +22,16 @@ class MerchantPresentedEncoder {
 
         if (payloadFormatIndicator.isNotBlank()) {
             dataField[data["payloadFormatIndicator"].toString()] = payloadFormatIndicator
+        } else {
+            throw Exception("payloadFormatIndicator is required")
         }
         if (pointOfInitiationMethod.isNotBlank()) {
             dataField[data["pointOfInitiationMethod"].toString()] = pointOfInitiationMethod
         }
         if (merchantCategoryCode.isNotBlank()) {
             dataField[data["merchantCategoryCode"].toString()] = merchantCategoryCode
+        } else {
+            throw Exception("merchantCategoryCode is required")
         }
         if (this::merchantAccountInformation.isInitialized) {
             dataField[merchantAccountInformation.tag] = merchantAccountInformation.value
@@ -34,18 +40,26 @@ class MerchantPresentedEncoder {
         }
         if (merchantCity.isNotBlank()) {
             dataField[data["merchantCity"].toString()] = merchantCity
+        } else {
+            throw Exception("merchantCity is required")
         }
         if (merchantName.isNotBlank()) {
             dataField[data["merchantName"].toString()] = merchantName
+        } else {
+            throw Exception("merchantName is required")
         }
         if (countryCode.isNotBlank()) {
             dataField[data["countryCode"].toString()] = countryCode
+        } else {
+            throw Exception("countryCode is required")
         }
         if (transactionAmount.isNotBlank()) {
             dataField[data["transactionAmount"].toString()] = transactionAmount
         }
         if (transactionCurrency.isNotBlank()) {
             dataField[data["transactionCurrency"].toString()] = transactionCurrency
+        } else {
+            throw Exception("transactionCurrency is required")
         }
         if (this::additionalDataFieldTemplate.isInitialized) {
             dataField[data["additionalDataFieldTemplate"].toString()] = additionalDataFieldTemplate.toString()
